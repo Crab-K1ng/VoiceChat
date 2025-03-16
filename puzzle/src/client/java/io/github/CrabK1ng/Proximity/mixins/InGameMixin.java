770ec4fb5e8dca012219a8447d3fc907440b7ec8
Spicylemon2623 <spicylemon2623@gmail.com>
2025-03-16 19:38:20 +0000

    Revert

--- puzzle/src/client/java/io/github/CrabK1ng/Proximity/mixins/InGameMixin.java
+++ puzzle/src/client/java/io/github/CrabK1ng/Proximity/mixins/InGameMixin.java
@@ -1,14 +1,14 @@
 package io.github.CrabK1ng.Proximity.mixins;
 
-import finalforeach.cosmicreach.gamestates.GameState;
+import finalforeach.cosmicreach.gamestates.InGame;
 import io.github.CrabK1ng.Proximity.ClientInitializer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(GameState.class)
-public class GameStateMixin {
+@Mixin(InGame.class)
+public class InGameMixin {
     @Inject(method = "onSwitchTo", at = @At("TAIL"))
     public void onSwitchTo(CallbackInfo ci) {
         ClientInitializer.captureAudio();
