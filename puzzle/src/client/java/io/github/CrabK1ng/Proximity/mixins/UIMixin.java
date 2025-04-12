91d412120bac5160982a6d4d7414f06f9ea770e5
Spicylemon2623 <spicylemon2623@gmail.com>
2025-04-12 21:17:51 +0100

    updates

--- puzzle/src/client/java/io/github/CrabK1ng/Proximity/mixins/UIMixin.java
+++ puzzle/src/client/java/io/github/CrabK1ng/Proximity/mixins/UIMixin.java
@@ -7,7 +7,6 @@ import finalforeach.cosmicreach.gamestates.GameState;
 import finalforeach.cosmicreach.gamestates.InGame;
 import finalforeach.cosmicreach.rendering.GameTexture;
 import finalforeach.cosmicreach.ui.UI;
-import io.github.CrabK1ng.Proximity.Constants;
 import io.github.CrabK1ng.Proximity.Proximity;
 import io.github.CrabK1ng.Proximity.VoiceMenu;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,10 +14,6 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import javax.sound.sampled.*;
-
-import java.io.IOException;
 
 import static finalforeach.cosmicreach.gamestates.GameState.currentGameState;
 import static io.github.CrabK1ng.Proximity.ProximityControls.openVoiceMenu;
@@ -32,14 +27,19 @@ public abstract class UIMixin {
     @Inject(method = "render", at = @At("HEAD"))
     private void drawIcon(CallbackInfo ci) {
         if (UI.renderUI && Proximity.drawIcon) {
+            //texture loading
             Texture micOn = GameTexture.load("proximity:mic.png").get();
             Texture micOff = GameTexture.load("proximity:mic_off.png").get();
+
+            //swapping to correct texture
             Sprite statusIcon = new Sprite(micOn);
             if (!Proximity.lineOpen) {statusIcon.setTexture(micOff);}
 
+            //set positions
             statusIcon.flip(false,true);
             statusIcon.setPosition(-uiViewport.getWorldWidth()/2+3, (uiViewport.getWorldHeight()/2) -32 -3);
 
+            //drawing to screen
             UI.batch.setProjectionMatrix(this.uiViewport.getCamera().combined);
             UI.batch.begin();
             Proximity.initText();
@@ -49,7 +49,7 @@ public abstract class UIMixin {
     }
 
     @Inject(method = "render", at = @At("HEAD"))
-    public void onKey(CallbackInfo ci) throws LineUnavailableException, IOException {
+    public void onKey(CallbackInfo ci) {
         if (toggleMute.isJustPressed()) {
             Proximity.toggleMic();
         }
@@ -57,22 +57,10 @@ public abstract class UIMixin {
             Proximity.toggleMenu();
         }
         if (Proximity.menuOpen && !(currentGameState instanceof VoiceMenu)) {
-            if (InGame.getLocalPlayer() != null) {
-                try {
-                    GameState.switchToGameState(new VoiceMenu());
-                } catch (Exception e) {
-                    Constants.LOGGER.info(e);
-                }
-            }
+            GameState.switchToGameState(new VoiceMenu());
         }
         if (!Proximity.menuOpen && currentGameState instanceof VoiceMenu) {
-            if (InGame.getLocalPlayer() != null) {
-                try {
-                    VoiceMenu.switchToGameState(new InGame());
-                } catch (Exception e) {
-                    Constants.LOGGER.info(e);
-                }
-            }
+            VoiceMenu.switchToGameState(new InGame());
         }
     }
 }
